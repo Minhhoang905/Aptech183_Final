dcdd745f8346c465ac3aedd34213c32e9f385402
Dao Nguyen Minh Hoang <minhhoang905@gmail.com>
2025-01-14 20:54:53 +0700

    commit test

--- src/main/java/com/example/Aptech_Final/Controller/HomeController.java
+++ src/main/java/com/example/Aptech_Final/Controller/HomeController.java
@@ -11,6 +11,7 @@ public class HomeController {
 	// Phương thức lấy cequestMapping(path)ác thông tin ở màn hình home
 	@RequestMapping(path = "/home", method = RequestMethod.GET)
 	public String getHome () {
+		  
 		
 		return "Demo";
 	}
