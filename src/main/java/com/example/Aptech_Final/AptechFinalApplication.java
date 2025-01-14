a84ab3a1c8479b9bce15ab30f93913aeb2daa98b
Dao Nguyen Minh Hoang <minhhoang905@gmail.com>
2025-01-14 20:48:58 +0700

    first commit

---
+++ src/main/java/com/example/Aptech_Final/AptechFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Aptech_Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptechFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptechFinalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Aptech_Final/Controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.Aptech_Final.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+
+public class HomeController {
+	
+	// Phương thức lấy cequestMapping(path)ác thông tin ở màn hình home
+	@RequestMapping(path = "/home", method = RequestMethod.GET)
+	public String getHome () {
+		
+		return "Demo";
+	}
+}
