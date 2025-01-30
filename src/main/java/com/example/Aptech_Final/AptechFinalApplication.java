b824431bbd755a6a5dbc7a01630c17d619e21eb5
Dao Nguyen Minh Hoang <minhhoang905@gmail.com>
2025-01-30 17:17:46 +0700

    Create Security, entity, controller

--- src/main/java/com/example/Aptech_Final/AptechFinalApplication.java
+++ src/main/java/com/example/Aptech_Final/AptechFinalApplication.java
@@ -2,6 +2,12 @@ package com.example.Aptech_Final;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class AptechFinalApplication {
@@ -9,5 +15,4 @@ public class AptechFinalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AptechFinalApplication.class, args);
 	}
-
 }
